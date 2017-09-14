a414bc915091d27a042a46e6870dc0e94f7d384c
陈烨彬 Robin Chen <xianyuxmu@gmail.com>
2017-09-15 00:27:05 +0800

    add Redis

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 const Koa = require('koa');
 const app = new Koa();
+const os = require('os'),
+      hostname = os.hostname();
+var redis = require("redis");
+
+// You can also use node_redis with promises: https://github.com/NodeRedis/node_redis#promises
+var bluebird = require('bluebird');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+var redisClient = redis.createClient();
+let redisAvailable = false;
+const PORT = 3000;
+let visits = 0;
+
+redisClient.on("error", (err) => {
+    redisAvailable = false;
+    console.log("Redis Error " + err);
+});
+
+redisClient.on('ready', () => {
+  redisAvailable = true;
+ console.log("Redis is ready");
+});
+
+const getCache = async (key) => {
+  if(!redisAvailable){
+    return 'Redis is not available!';
+  }
+  let data = await redisClient.getAsync(key);
+  if(data) {
+    data = JSON.parse(data.toString());
+  }
+  return data;
+};
 
 app.use(async ctx => {
   console.log('A Request!');
-  ctx.body = 'Bonjour Monde!';
+  try {
+    if(redisAvailable) {
+      redisClient.incr('visits');
+    }
+    visits = await getCache('visits');
+  }
+  catch(e) {
+    visits = 'Redis Error!';
+    console.log('redisClient.incr() Error: ', e);
+  }
+  
+
+  ctx.body = `Bonjour Monde! Hostname: ${hostname}, site visits from Redis: ${visits}`;
 });
 
-console.log('APP START!!!');
-app.listen(3000);
+console.log('APP START!!! App is running on port: ', PORT);
+app.listen(PORT);
